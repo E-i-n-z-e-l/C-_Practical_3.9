6f0a12126ebadefc48fa4b6c408726901e1c5577
Einzel <einzel0102@mail.ru>
2023-02-02 20:08:47 +0800

    Рекурсия. Задача 69: Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B

---
+++ Task69/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 69: Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3 в 5 степени)
+// A = 2; B = 3 -> 8
+
+Console.WriteLine("Введите первое натуральное число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе натуральное число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > 0 & number2 > 0) Console.WriteLine(StepenDigit(number1, number2));
+else Console.WriteLine("Введите положительные числа: ");
+
+
+int StepenDigit (int num1, int num2)
+{
+	if (num2 == 0) return 1;
+	else return num1 * StepenDigit(num1, num2 - 1);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
